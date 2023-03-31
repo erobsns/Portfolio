6dcdb86e6ee2a624150c03fbe912f1afbd9ac721
erobsns <83842059+erobsns@users.noreply.github.com>
2023-03-31 12:12:03 -0700

    Delete mapIt.py

--- mapIt.py
+++
@@ -1,9 +0,0 @@
-#!python3
-#mapIt.py - Launches a map in the browser using an address/name of place from the
-#clipboard, you could add it to a hotbar etc for quick mapping.
-import webbrowser,pyperclip
-
-#get address/name of place from clipboard
-address=pyperclip.paste()
-
-webbrowser.open('https://www.google.com/maps/place/'+address)
